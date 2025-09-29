cda1f361f28c18123a4e0e4bcbf70d076f1edc26
thecomediandan <danishpurplesnake@gmail.com>
2025-09-29 11:50:21 -0400

    feat: El formulario modifica el DOM como se lo espera

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2, signal, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, Renderer2, signal, ViewChild } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
@@ -19,18 +19,18 @@ export class App{
   formControl: FormGroup;
 
   private containerWidth: number = 594;
-  numberColumns = signal(4);
-  numberRows = signal(4);
-  union = signal(false);
+  numberColumns: number = 4;
+  numberRows: number = 4;
+  union: boolean = false;
   private gap: number = 4;
   private cellSize!: number;
 
   constructor(private renderer2: Renderer2, private formBuilder: FormBuilder) {
     // Ideal para inyecciones de dependencias pero no para lógica que dependa del DOM o de los inputs
     this.formControl = this.formBuilder.group({
-    dimx: [`${this.numberColumns()}`, [Validators.required, Validators.min(1), Validators.max(10)]],
-    dimy: [`${this.numberRows()}`, [Validators.required, Validators.min(1), Validators.max(10)]],
-    union: [false, [Validators.required]]
+    dimx: [`${this.numberColumns}`, [Validators.required, Validators.min(1), Validators.max(10)]],
+    dimy: [`${this.numberRows}`, [Validators.required, Validators.min(1), Validators.max(10)]],
+    union: [this.union, [Validators.required]]
     });
   }
 
@@ -46,17 +46,21 @@ export class App{
 
   calculateCellSize(): void {
     this.containerWidth = this.gridContainer.nativeElement.clientWidth as number;
-    this.cellSize = (this.containerWidth - (this.numberColumns() - 1) * this.gap) / this.numberColumns();
+    this.cellSize = (this.containerWidth - (this.numberColumns - 1) * this.gap) / this.numberColumns;
   }
 
   fillGridContainer(): void {
+    this.renderer2.setProperty(this.gridContainer.nativeElement, 'innerHTML', '');
     this.calculateCellSize();
     this.renderer2.setStyle(this.gridContainer.nativeElement, 'grid-template-columns', `repeat(${this.numberColumns}, ${this.cellSize}px)`);
     this.renderer2.setStyle(this.gridContainer.nativeElement, 'grid-template-rows', `repeat(${this.numberRows}, ${this.cellSize}px)`);
     this.renderer2.setStyle(this.gridContainer.nativeElement, 'gap', `${this.gap}px`);
-    for (let i = 0; i < this.numberColumns() * this.numberRows(); i++) {
+    for (let i = 0; i < this.numberColumns * this.numberRows; i++) {
       const cell = this.renderer2.createElement('div');
       this.renderer2.addClass(cell, 'grid-item');
+      if (this.union) {
+        this.renderer2.addClass(cell, 'checked');
+      }
       this.renderer2.setStyle(cell, 'width', `${this.cellSize}px`);
       this.renderer2.setStyle(cell, 'height', `${this.cellSize}px`);
       // const text = this.renderer2.createText(`Elemento ${i + 1}`);
@@ -65,21 +69,29 @@ export class App{
     }
   }
 
-  onsubmit(): void {
+  onSubmit(): void {
     if (this.formControl.invalid) {
       return;
     }
-    this.numberColumns.set(this.formControl.value.dimx as number);
-    this.numberRows.set(this.formControl.value.dimy as number);
+    this.numberColumns = this.formControl.value.dimx as number;
+    this.numberRows = this.formControl.value.dimy as number;
+    this.union = this.unionCheckbox.nativeElement.checked as boolean;
 
-    if (this.unionCheckbox.nativeElement.checked) {
-      console.log("checked");
-      this.union.set(true);
-    }else {
-      console.log("unchecked");
-      this.union.set(false);
-    }
-
+    // if (this.unionCheckbox.nativeElement.checked as boolean) {
+    //   console.log("checked");
+    //   this.union = true;
+    // }else {
+    //   console.log("unchecked");
+    //   this.union = false;
+    // }
+    console.log(this.unionCheckbox);
     console.log(this.formControl.value);
+    this.fillGridContainer();
+  }
+
+  onChangeCheckbox(): void {
+    this.union = this.unionCheckbox.nativeElement.checked as boolean;
+    console.log("changed: ", this.unionCheckbox.nativeElement.checked as boolean);
+    this.fillGridContainer();
   }
 }
